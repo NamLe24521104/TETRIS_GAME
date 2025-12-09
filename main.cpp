67e0ae5b7144812b42f0cb3cd99d7c812147cd5f
Lê Hoài Nam <24521104@gm.uit.edu.vn>
2025-12-09 14:48:24 +0700

    Create project skeleton with function stubs

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+// --- KHAI BAO BIEN TOAN CUC (TV1 & TV5) ---
+// (De trong cho TV5 dien)
+
+// --- HAM TIEN ICH (TV2 & TV4) ---
+void gotoxy(int x, int y) { /* TV2 se lam */ }
+void draw() { /* TV2 se lam */ }
+void enableRawMode() { /* TV4 se lam */ }
+void input() { /* TV4 se lam */ }
+
+// --- HAM LOGIC GAME (TV3) ---
+bool canMove() { return false; /* TV3 se lam */ }
+void logic() { /* TV3 se lam */ }
+
+// --- MAIN (TV1) ---
+int main() {
+    cout << "Du an Tetris Nhom 5 khoi dong...";
+    return 0;
+}
